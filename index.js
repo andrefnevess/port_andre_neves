9c9a43dba6579f1e39e43114faee835bd9a5d8b9
andrefnevess <andrefneves30@gmail.com>
2025-08-18 17:56:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const scrollTopBtn = document.getElementById('scrollTopBtn');
 
+window.onscroll = () => {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "flex";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+function aumentarFonte() {
+  document.body.style.fontSize = "larger";
+}
+function diminuirFonte() {
+  document.body.style.fontSize = "smaller";
+}
