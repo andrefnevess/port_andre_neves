1c2857a5df933a28811ac94f54f93b6302c1b655
andrefnevess <andrefneves30@gmail.com>
2025-08-19 17:46:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -21,3 +21,68 @@ function aumentarFonte() {
 function diminuirFonte() {
   document.body.style.fontSize = "smaller";
 }
+
+// Efeito folha mouse
+
+const num_folhas = 5;
+const folhas = [];
+
+for (let i = 0; i < num_folhas; i++) {
+  const img = document.createElement("img");
+  img.src = "folha.png"; 
+  img.classList.add("folha");
+  document.body.appendChild(img);
+  folhas.push({element: img, x: 0, y: 0});
+}
+
+let mouseX = 0;
+let mouseY = 0;
+
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+
+function animate() {
+  let x = mouseX;
+  let y = mouseY;
+
+  folhas.forEach((folhas, index) => {
+    folhas.x += (x - folhas.x) * 0.1; // suaviza o movimento
+    folhas.y += (y - folhas.y) * 0.1;
+
+    folhas.element.style.transform = translate(${folhas.x}px, ${folhas.y}px);
+
+    // cada estrela segue um pouco atrás da anterior
+    x = folhas.x;
+    y = folhas.y;
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+
+function animate() {
+  let prevX = mouseX;
+  let prevY = mouseY;
+
+  stars.forEach((starObj, i) => {
+    const speed = 0.15 - i*0.008;
+    starObj.x += (prevX - starObj.x) * speed;
+    starObj.y += (prevY - starObj.y) * speed;
+
+    starObj.el.style.left = ${starObj.x - parseFloat(starObj.el.style.width)/2}px;
+    starObj.el.style.top = ${starObj.y - parseFloat(starObj.el.style.height)/2}px;
+
+    prevX = starObj.x;
+    prevY = starObj.y;
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
